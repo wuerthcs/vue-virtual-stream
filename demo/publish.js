3496824db4105725f796adc0428074e29f5c8921
Dominik Biedebach <6538827+bdbch@users.noreply.github.com>
2018-12-12 23:48:04 +0100

    fix(demo): fixed deploy config

--- demo/publish.js
+++
@@ -1,8 +0,0 @@
-const ghpages = require('gh-pages')
-
-ghpages.publish('./dist', (err) => {
-    if (err) {
-        console.error('The demo could not be published to github')
-        console.error(err)
-    }
-})
